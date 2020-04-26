18bd527079ec0456f1625248eac48876dab5b037
YungPingXu <a0976373843@gmail.com>
2020-04-26 21:52:04 +0800

    reuploaded

--- NCTU OJ/Assignment 5/477 . Assignment 5 (Week 5) - Heap.cpp
+++ NCTU OJ/Assignment 5/477 . Assignment 5 (Week 5) - Heap.cpp
@@ -4,31 +4,37 @@ using namespace std;
 int main() {
     int n, m;
     scanf("%d%d", &n, &m);
-    priority_queue<int, vector<int>, greater<int>> p[n];
+    vector<priority_queue<int, vector<int>, greater<int>>*> p(1 + n);
+    for (int i = 1; i <= n; i++) p[i] = new priority_queue<int, vector<int>, greater<int>>();
     while (m--) {
         char s[8];
         int i, j;
         scanf("%s", s);
         if (s[0] == 'i') {
             scanf("%d%d", &i, &j);
-            p[i - 1].push(j);
+            p[i]->push(j);
         } else if (s[0] == 'j') {
             scanf("%d%d", &i, &j);
-            while (!p[j - 1].empty()) {
-                p[i - 1].push(p[j - 1].top());
-                p[j - 1].pop();
+            if (p[j]->size() > p[i]->size()) {
+                auto tmp = p[j];
+                p[j] = p[i];
+                p[i] = tmp;
+            } // 當p[j]的大小比p[i]大很多時，會是非常耗時的操作，所以把它變成從小的移到大的
+            while (!p[j]->empty()) {
+                p[i]->push(p[j]->top());
+                p[j]->pop();
             }
         } else if (s[0] == 'e') {
-            scanf("%d%d", &i, &j);
-            if (!p[i - 1].empty()) {
-                printf("%d\n", p[i - 1].top());
-                p[i - 1].pop();
+            scanf("%d", &i);
+            if (!p[i]->empty()) {
+                printf("%d\n", p[i]->top());
+                p[i]->pop();
             } else
                 puts("NULL");
         } else if (s[0] == 'l') {
             scanf("%d", &i);
-            if (!p[i - 1].empty())
-                printf("%d\n", p[i - 1].top());
+            if (!p[i]->empty())
+                printf("%d\n", p[i]->top());
             else
                 puts("NULL");
         }
